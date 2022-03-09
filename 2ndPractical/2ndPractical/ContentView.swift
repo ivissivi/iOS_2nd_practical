9f5aa6a18f11bda33f23e32e9d81174d80061715
ivissivi <ivis31121999@gmail.com>
2022-03-09 21:25:47 +0100

    Add files via upload

--- 2ndPractical/2ndPractical/ContentView.swift
+++ 2ndPractical/2ndPractical/ContentView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    var trafficLightColors: [Color] = [.red, .orange, .green]
+    
+    @State var currentTrafficLight = Color.red
+    
     var body: some View {
+        
         Text("Luksofors App")
             .padding()
         
-        HStack {
+        VStack {
+            
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: {})
+                .datePickerStyle(CompactDatePickerStyle())
+                .clipped()
+                .labelsHidden()
             Circle()
                 .fill(Color.red)
+                .frame(width: 100, height: 100)
+            Circle()
+                .fill(Color.orange)
+                .frame(width: 100, height: 100)
+            Circle()
+                .fill(Color.green)
                 .frame(width: 100, height: 100)
         }
     }
